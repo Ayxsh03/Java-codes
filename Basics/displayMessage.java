0f9740d099136794b8c652ca98ad7dd203afae75
AYUSH SAI <152642328+Ayxsh03@users.noreply.github.com>
2024-04-25 13:31:53 +0530

    Create displayMessage.java

---
+++ Basics/displayMessage.java
@@ -0,0 +1,16 @@
+/*
+Problem statement
+Write a program to display a welcome message on the screen.
+Welcome Message:
+Welcome to Java course!
+Hello
+Ninja
+*/
+package Basics;
+public class displayMessage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java course!");
+        System.out.println("Hello");
+        System.out.println("Ninja");
+    }    
+}
