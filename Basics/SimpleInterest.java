5a17d16b25d61368eaf2bac3a307cb3952f6cf52
AYUSH SAI <152642328+Ayxsh03@users.noreply.github.com>
2024-04-25 16:34:01 +0530

    Create SimpleInterest.java

---
+++ Basics/SimpleInterest.java
@@ -0,0 +1,16 @@
+/*
+Take the principal amount, rate of interest, and the time period as input and calculate the Simple Interest.
+Note: Print the answer as integer value.
+ */
+package Basics;
+import java.util.Scanner;
+public class SimpleInterest {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        float p=sc.nextFloat();
+        float r=sc.nextFloat();
+        float t=sc.nextFloat();
+        int si=(int)((p*r*t)/100);
+        System.out.println(si);
+    }
+}
