a900bd8dd35873176321256b538db367ca1a383b
AYUSH SAI <152642328+Ayxsh03@users.noreply.github.com>
2024-04-26 11:49:11 +0530

    Create output.java

---
+++ CondnLoops/output.java
@@ -0,0 +1,14 @@
+package CondnLoops;
+
+public class output {
+    public static void main(String args[])
+    {
+        int var1 = 1; 
+        int var2 = 6;
+        if ((var2 = 1) == var1)
+            System.out.print(var2);
+        else 
+            System.out.print(var2 + 1);
+    } 
+    
+}
