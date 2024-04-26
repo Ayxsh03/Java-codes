840c72ab8b1c176a649719fc5c217fab14c0e9c2
AYUSH SAI <152642328+Ayxsh03@users.noreply.github.com>
2024-04-26 12:38:56 +0530

    Create EvenSum.java

---
+++ CondnLoops/EvenSum.java
@@ -0,0 +1,20 @@
+//Given a number N, print sum of all even numbers from 1 to N.
+package CondnLoops;
+import java.util.*;
+public class EvenSum {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int sum=0;
+        for(int i=0;i<=n;i++){
+            sum=sum+i;
+            i++;
+        }
+        // int j=2;
+        // while (j<=n) {
+        //     sum=sum+j;
+        //     j+=2;
+        // }
+        System.out.println(sum);
+    }
+}
