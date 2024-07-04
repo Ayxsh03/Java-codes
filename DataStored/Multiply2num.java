f7b2a5eb3b4f44ecc1a3b1966c9f7d985af4bc94
AYUSH SAI <152642328+Ayxsh03@users.noreply.github.com>
2024-07-04 22:47:52 +0530

    Create Multiply2num.java

---
+++ DataStored/Multiply2num.java
@@ -0,0 +1,13 @@
+// Write a code which takes two integers "F1" and "F2" as inputs and print their product .
+
+package DataStored;
+import java.util.Scanner;
+public class Multiply2num {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        long f1=sc.nextLong();
+        long f2=sc.nextLong();
+        System.out.println(f1*f2);
+    }
+    
+}
