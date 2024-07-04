43c2942a30ca6b5a3e8ae6e864fe9443b463fddc
AYUSH SAI <152642328+Ayxsh03@users.noreply.github.com>
2024-07-04 22:53:15 +0530

    Create ASCIIval.java

---
+++ DataStored/ASCIIval.java
@@ -0,0 +1,15 @@
+//Given a character, you need to print the ASCII value of that character.
+
+package DataStored;
+
+import java.util.Scanner;
+
+public class ASCIIval {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        char c=sc.next().charAt(0);
+        int i= c;
+        System.out.println(i);
+    }
+    
+}
