90f4a49f5a3f1d1c99147c05a530e4487183c261
AYUSH SAI <152642328+Ayxsh03@users.noreply.github.com>
2024-07-12 22:52:01 +0530

    Basic operation

---
+++ Arrays/operation.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+public class operation {
+    public class operations {
+        public static void main(String[] args) {
+            //Declaration of array
+            int[] arr=new int[20];
+            //accessing element at index
+            arr[3]=30;
+            System.out.println(arr[3]);
+            
+            char[] ch= new char[10];
+            ch[1]='a';
+            System.out.println(ch[1]);
+        }
+    
+    }   
+}
