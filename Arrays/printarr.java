b4d71ec62a7da48249c507a44e7e78ed5bef2c7a
AYUSH SAI <152642328+Ayxsh03@users.noreply.github.com>
2024-07-12 23:08:52 +0530

    Functions added

--- Arrays/printarr.java
+++ Arrays/printarr.java
@@ -1,16 +1,26 @@
 package Arrays;
 import java.util.*;
+
 public class printarr {
-    public static void main(String[] args) {
+    
+    public static int[] takeInput(){
         Scanner sc=new Scanner(System.in);
         int size=sc.nextInt();
         int[] arr=new int[size];
         for(int i=0;i<size;i++){
             arr[i]=sc.nextInt();
         }
-        for (int i = 0; i < size; i++) {
+        return arr;
+    }
+    
+    public static void printArray(int[] arr){
+        for (int i = 0; i < arr.length ; i++) {
             System.out.print(arr[i]+ " "); 
         }
     }
     
+    public static void main(String[] args) {
+       int[] arr=takeInput();
+       printArray(arr);
+    }  
 }
