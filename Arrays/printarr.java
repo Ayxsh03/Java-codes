f5893af20f070a66f113a73f034f9b61d0605243
AYUSH SAI <152642328+Ayxsh03@users.noreply.github.com>
2024-07-13 14:46:22 +0530

    Largest element function

--- Arrays/printarr.java
+++ Arrays/printarr.java
@@ -19,8 +19,21 @@ public class printarr {
         }
     }
     
+    public static int max(int[]arr){
+        int max=Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        return max;
+
+    }
     public static void main(String[] args) {
        int[] arr=takeInput();
        printArray(arr);
+       System.out.println();
+       int largest=max(arr);
+       System.out.println("Largest= "+largest);
     }  
 }
