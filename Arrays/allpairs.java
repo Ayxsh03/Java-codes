e20db6ffbcbd19b2e3c51355e68e65d1075b282c
AYUSH SAI <152642328+Ayxsh03@users.noreply.github.com>
2024-07-16 15:18:59 +0530

    new

---
+++ Arrays/allpairs.java
@@ -0,0 +1,26 @@
+package Arrays;
+import java.util.*;
+public class allpairs {
+    public static int[] takeInput(){
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        int[] arr=new int[size];
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+        return arr;
+    }
+    public static void pairs(int []arr){
+        for (int i = 0; i < arr.length; i++) {
+            for(int j=i+1; j<arr.length; j++){
+                System.out.print("(" +arr[i] +","+arr[j]+")");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int[] arr=takeInput();
+        pairs(arr);
+    }
+
+}
---
+++ Arrays/swapalternate.java
@@ -0,0 +1,40 @@
+//You have been given an array/list(ARR) of size N. You need to swap every pair of alternate
+//You don't need to print or return anything, just change in the input array itself.
+
+package Arrays;
+
+import java.util.Scanner;
+
+public class swapalternate {
+    public static int[] takeInput(){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        return arr;
+    }
+    public static void alternate(int[] arr){
+        for (int i = 0; i < arr.length-1; i=i+2) {
+            int temp=arr[i];
+            arr[i]=arr[i+1];
+            arr[i+1]=temp;
+            
+        }
+    }
+    public static void printArray(int[] arr){
+        for (int i = 0; i < arr.length ; i++) {
+            System.out.print(arr[i]+ " "); 
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int w=sc.nextInt();
+        for (int i = 0; i < w; i++) {
+            int []arr=takeInput();
+            alternate(arr);
+            printArray(arr);
+        }  
+    }
+}
