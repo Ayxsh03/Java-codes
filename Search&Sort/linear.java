61d7ee5ecb33f919fd0ffc439715e97b9fe39f51
AYUSH SAI <152642328+Ayxsh03@users.noreply.github.com>
2024-07-16 16:00:19 +0530

    Linear Search

---
+++ Search&Sort/linear.java
@@ -0,0 +1,28 @@
+
+
+import java.util.*;
+public class linear {
+    public static int[] takeInput(){
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        int[] arr=new int[size];
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+        return arr;
+    }
+    public static void linearSearch(int[]arr, int key){
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]==key){
+                System.out.println("element found at index "+ i);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int []arr=takeInput();
+        int key=sc.nextInt();
+        linearSearch(arr, key);
+    }
+    
+}
