371d542fd527fd8c6490cc8cc906401c998f6db5
AYUSH SAI <152642328+Ayxsh03@users.noreply.github.com>
2024-07-18 11:30:31 +0530

    comment

--- Strings/reversewords.java
+++ Strings/reversewords.java
@@ -1,19 +1,5 @@
 /*Reverse the given string word wise. That is, the last word in given string should come at 1st place,
 last second word at 2nd place and so on. Individual words should remain as it is.
-
-Psedocode
-Function reverseStringWordWise (input) :
-    Initialize previndex as -1, i as 0
-    While input [i] != `\O': //special character at the end of each string
-        If input (1) == ' ':
-            reverse (input, prevIndex + 1, i - 1) // Reverse the word between prevIndex and i-1
-            Set prevIndex to i
-        Increment i
-    Call reverse (input, prevIndex + 1, i - 1) // Reverse the last word
-    Call reverse (input, 0, i - 1) // Reverse the entire string
-Function reverse (input, start, end):
-    While start < end:
-        Swap input[start] with input [end]
  */
 package Strings;
 public class reversewords {
@@ -26,7 +12,7 @@ public class reversewords {
                 end=i;
             }
         }
-        reversed=reversed+input.substring(0, end);
+        reversed=reversed+input.substring(0, end);//For first word
         
         return reversed;
     }
