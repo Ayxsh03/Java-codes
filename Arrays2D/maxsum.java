126b3a5ab8df78f5dff575f57dadf4c6c225a8e4
AYUSH SAI <152642328+Ayxsh03@users.noreply.github.com>
2024-07-18 21:18:46 +0530

    new function

--- Arrays2D/maxsum.java
+++ Arrays2D/maxsum.java
@@ -34,6 +34,45 @@ public class maxsum {
         }
         return largestrow;
     }
+    //Combined function
+	public static void findLargest(int arr[][]){
+        boolean isRow = true;
+        int largestSum = Integer.MIN_VALUE;
+        int rows = arr.length;
+        int index = 0;
+        int cols= arr[0].length;
+        //Program to calculate RowSum
+        for(int i=0;i<rows;i++){
+            int rowSum = 0;
+            for(int j=0;j<cols;j++){
+               rowSum = rowSum + arr[i][j];
+            }
+            if(rowSum>largestSum){
+                largestSum = rowSum;
+                index = i;
+            }
+        }
+        //Program to calculate ColSum
+        for(int j=0;j<cols;j++){
+            int colSum = 0;
+            for(int i=0;i<rows;i++){
+                colSum = colSum + arr[i][j];
+            }
+            if(colSum>largestSum){
+                largestSum = colSum;
+                index = j;
+                isRow = false; //to show colSum is greater than RowSum
+            }
+        }
+        //Now check which has higher sum row or column
+        if(isRow){
+            System.out.println("row "+index+ " "+largestSum);
+        }else{
+            System.out.println("column "+ index+ " "+ largestSum);
+        }
+    }
+
+
     public static void main(String[] args) {
         int arr[][]={{5,6,7},{6,4,9},{8,5,9}};
         int rowsum=largestrowsum(arr);
@@ -43,5 +82,6 @@ public class maxsum {
         }else{
             System.out.println(colsum+" col");
         }
+        findLargest(arr);
     }
 }
