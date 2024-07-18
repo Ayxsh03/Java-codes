9e23874da2db136ebf1613d826687211ea703981
AYUSH SAI <152642328+Ayxsh03@users.noreply.github.com>
2024-07-18 21:14:24 +0530

    Largest Sum of row/col

---
+++ Arrays2D/maxsum.java
@@ -0,0 +1,47 @@
+//For a given two-dimensional integer array/list of size (N x M), you need to find out which row or column 
+//has the largest sum
+
+package Arrays2D;
+
+public class maxsum {
+    public static int largestcolsum(int arr[][]){
+        int rows=arr.length;
+        int cols=arr[0].length;
+        int largestcol=Integer.MIN_VALUE;
+        for(int j=0;j<cols;j++){
+            int sum=0;
+            for(int i=0;i<rows;i++){
+                sum=sum+arr[i][j];
+            }
+            if(sum>largestcol){
+                largestcol=sum;
+            }
+        }
+        return largestcol;
+    }
+    public static int largestrowsum(int arr[][]){
+        int rows=arr.length;
+        int cols=arr[0].length;
+        int largestrow=Integer.MIN_VALUE;
+        for(int i=0;i<rows;i++){
+            int sum=0;
+            for(int j=0;j<cols;j++){
+                sum=sum+arr[i][j];
+            }
+            if(sum>largestrow){
+                largestrow=sum;
+            }
+        }
+        return largestrow;
+    }
+    public static void main(String[] args) {
+        int arr[][]={{5,6,7},{6,4,9},{8,5,9}};
+        int rowsum=largestrowsum(arr);
+        int colsum=largestcolsum(arr);
+        if(rowsum>colsum){
+            System.out.println(rowsum+" row");
+        }else{
+            System.out.println(colsum+" col");
+        }
+    }
+}
