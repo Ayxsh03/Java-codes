8a361c82fb981f0905d8733334a3786e9f3985fb
AYUSH SAI <152642328+Ayxsh03@users.noreply.github.com>
2024-07-18 17:31:05 +0530

    square matrix

---
+++ Arrays2D/square.java
@@ -0,0 +1,24 @@
+package Arrays2D;
+
+import java.util.Scanner;
+
+public class square {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int rows=sc.nextInt();
+        int cols=sc.nextInt();
+        int[][] arr=new int[rows][cols];
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        if(rows==cols){
+            for(int i=0;i<rows;i++){
+                for(int j=0;j<cols;j++){
+                    System.out.println(arr[j][j]);
+                }
+            }
+        }
+    }
+}
