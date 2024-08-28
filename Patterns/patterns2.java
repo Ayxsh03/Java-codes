86c102f1cc32f8bca3ae1648e0037ece00a578d5
AYUSH SAI <152642328+Ayxsh03@users.noreply.github.com>
2024-08-28 19:55:10 +0530

    last

--- Patterns/patterns2.java
+++ Patterns/patterns2.java
@@ -1,5 +1,8 @@
 package Patterns;
 
+import DataStructures.LinkedList.segregate012;
+import Strings.stringbuffer;
+
 public class patterns2 {
     
     public static void numberCrown(int n){
@@ -332,7 +335,29 @@ public class patterns2 {
         //     System.out.println();
         // }
     }
+    
+    public static void numberPattern(int n){
+        /*
+        4 4 4 4 4 4 4 
+        4 3 3 3 3 3 4 
+        4 3 2 2 2 3 4 
+        4 3 2 1 2 3 4 
+        4 3 2 2 2 3 4 
+        4 3 3 3 3 3 4 
+        4 4 4 4 4 4 4 
+        */
+        for(int i=0;i<2*n-1;i++){
+            for(int j=0;j<2*n-1;j++){
+                int top=i;
+                int left=j;
+                int bottom=2*n-2-i;
+                int right=2*n-2-j;
+                System.out.print(n-Math.min(Math.min(top, bottom), Math.min(left, right))+" ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
-        incNumTriangle(5);
+        numberPattern(4);
     }
 }
