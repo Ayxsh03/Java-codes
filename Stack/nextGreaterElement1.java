d184594eeecdc6fbba75c106b8da4e2e8e25eced
AYUSH SAI <152642328+Ayxsh03@users.noreply.github.com>
2024-11-07 20:21:08 +0530

    s

---
+++ Stack/nextGreaterElement1.java
@@ -0,0 +1,68 @@
+/*
+The next greater element of some element x in an array is the first greater element that is to 
+the right of x in the same array.You are given two distinct 0-indexed integer arrays nums1 and nums2, 
+where nums1 is a subset of nums2.For each 0 <= i < nums1.length, find the index j such that 
+nums1[i] == nums2[j] and determine the next greater element of nums2[j] in nums2. If there is no 
+next greater element, then the answer for this query is -1.
+Return an array ans of length nums1.length such that ans[i] is the next greater element as described above.
+ */
+package Stack;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class nextGreaterElement1 {
+    
+    public static int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int nge[] = new int[n];
+        Stack < Integer > st = new Stack < > ();
+        for (int i = 2 * n - 1; i >= 0; i--) {
+            while (st.isEmpty() == false && st.peek() <= nums[i % n]) {
+                st.pop();
+            }
+
+            if (i < n) {
+                if (st.isEmpty() == false) nge[i] = st.peek();
+                else nge[i] = -1;
+            }
+
+            st.push(nums[i % n]);
+        }
+        return nge;
+    }
+
+    public int[] nextGreaterElementI(int[] nums1, int[] nums2) {
+        if (nums2.length == 0 || nums1.length == 0)
+            return new int[0];
+        Map<Integer, Integer> numberNGE = new HashMap<>();
+        Stack<Integer> numStack = new Stack<>();
+
+        numStack.push(nums2[nums2.length - 1]);
+        numberNGE.put(nums2[nums2.length - 1], -1);
+
+        for (int i = nums2.length - 2; i >= 0; i--) {
+
+            if (nums2[i] < numStack.peek()) {
+                numberNGE.put(nums2[i], numStack.peek());
+                numStack.push(nums2[i]);
+                continue;
+            }
+
+            while (!numStack.isEmpty() && numStack.peek() < nums2[i])
+                numStack.pop();
+
+            if (numStack.isEmpty()) {
+                numStack.push(nums2[i]);
+                numberNGE.put(nums2[i], -1);
+            } else {
+                numberNGE.put(nums2[i], numStack.peek());
+                numStack.push(nums2[i]);
+            }
+        }
+        for (int i = 0; i < nums1.length; i++)
+            nums1[i] = numberNGE.get(nums1[i]);
+        return nums1;
+    }
+}
